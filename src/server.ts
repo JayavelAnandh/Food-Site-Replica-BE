e1f8ff9fb41a6ec83ca308484e77a10da0da23f8
Jayavel Anandh <jayavel.anandh@tolaram.com>
2025-10-05 14:05:41 +0530

    updated api

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import app from "./app";
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/",(req,res)=>{
+    res.status(200).send("Server is Listening")
+})
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
